a6a0af40b98c65b44dc937a0ce528f592c3e7179
Juheon Lee <juheonleewk@gmail.com>
2026-03-06 12:02:04 +0900

    Update LeeJuHeon5.py

--- LeeJuHeon5.py
+++ LeeJuHeon5.py
@@ -15,18 +15,15 @@ st.title("🌞 Prophet Forecast with Preprocessed Sunspot Data")
 # [1] 데이터 불러오기
 # ----------------------------------
 # TODO: 'sunspots_for_prophet.csv' 파일을 불러오고, 'ds' 컬럼을 datetime 형식으로 변환하세요.
-'''코드를 작성하시오'''
 df = pd.read_csv('sunspots_for_prophet.csv')
 df['ds'] = pd.to_datetime(df['ds'])
 
 st.subheader("📄 불러온 데이터 미리보기")
-'''코드를 작성하시오'''
-st.dataframe(data=df, width='stretch')
+st.dataframe(data=df.head(), width='stretch')
 # ----------------------------------
 # [2] Prophet 모델 정의 및 학습
 # ----------------------------------
 # TODO: Prophet 모델을 생성하고, 11년 주기 커스텀 seasonality를 추가한 후 학습하세요.
-'''코드를 작성하시오'''
 model = Prophet(
     yearly_seasonality=False,
     changepoint_prior_scale=0.05,
@@ -38,7 +35,6 @@ model.fit(df)
 # [3] 예측 수행
 # ----------------------------------
 # TODO: 30년간 연 단위 예측을 수행하고, 결과를 forecast에 저장하세요.
-'''코드를 작성하시오'''
 future = model.make_future_dataframe(periods=30, freq='YE')
 forecast = model.predict(future)
 # ----------------------------------
@@ -46,7 +42,6 @@ forecast = model.predict(future)
 # ----------------------------------
 st.subheader("📈 Prophet Forecast Plot")
 # TODO: model.plot()을 사용하여 예측 결과를 시각화하세요.
-'''코드를 작성하시오'''
 fig1 = model.plot(forecast)
 plt.title("Prophet Forecast Plot")
 plt.xlabel("ds")
@@ -55,7 +50,6 @@ st.pyplot(fig1)
 
 st.subheader("📊 Forecast Components")
 # TODO: model.plot_components()를 사용하여 구성요소를 시각화하세요.
-'''코드를 작성하시오'''
 fig2 = model.plot_components(forecast)
 st.pyplt(fig2)
 
@@ -67,7 +61,6 @@ st.subheader("📉 Custom Plot: Actual vs Predicted with Prediction Intervals")
 # TODO: 실제값, 예측값, 신뢰구간을 하나의 plot에 시각화하세요.
 fig3, ax = plt.subplots(figsize=(14, 6))
 
-'''코드를 작성하시오'''
 # 힌트:
 # ax.plot(df["ds"], df["y"], ...)
 # ax.plot(forecast["ds"], forecast["yhat"], ...)
@@ -89,7 +82,6 @@ st.pyplot(fig3)
 st.subheader("📉 Residual Analysis (예측 오차 분석)")
 
 # TODO: df와 forecast를 'ds' 기준으로 병합하여 residual 컬럼을 생성하세요.
-'''코드를 작성하시오'''
 merged = df_res.merge(forecast, how="inner", on = "ds")
 merged["residual"] = merged["y"] - merged["yhat"]
 
@@ -105,7 +97,6 @@ ax2.grid(True)
 
 plt.tight_layout()
 
-'''코드를 작성하시오'''
 # 힌트:
 # ax2.plot(merged["ds"], merged["residual"], ...)
 # ax2.axhline(0, ...)
@@ -117,5 +108,5 @@ st.pyplot(fig4)
 # ----------------------------------
 st.subheader("📌 Residual Summary Statistics")
 # TODO: merged["residual"].describe()를 출력하세요.
-'''코드를 작성하시오'''
 st.dataframe(data=merged["residual"].describe(), width='stretch')
+
