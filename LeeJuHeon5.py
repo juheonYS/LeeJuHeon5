25f4a9e21c12055c80bb4f3eb2d546878207b5b7
Juheon Lee <juheonleewk@gmail.com>
2026-03-06 12:05:47 +0900

    Update LeeJuHeon5.py

--- LeeJuHeon5.py
+++ LeeJuHeon5.py
@@ -87,7 +87,7 @@ merged["residual"] = merged["y"] - merged["yhat"]
 
 # TODO: residual 시계열을 시각화하세요.
 fig4, ax2 = plt.subplots(figsize=(14, 4))
-ax2.set_plot(merged["ds"], merged["residual"], color='purple', marker='o', linestyle='-', label='Residual')
+ax2.plot(merged["ds"], merged["residual"], color='purple', marker='o', linestyle='-', label='Residual')
 ax2.axhline(0, color = 'black', linestyle = '--')
 ax2.title("Residual Analysis (Actual - Predicted)")
 ax2.set_xlabel("Year")
@@ -109,6 +109,7 @@ st.pyplot(fig4)
 st.subheader("📌 Residual Summary Statistics")
 # TODO: merged["residual"].describe()를 출력하세요.
 st.dataframe(data=merged["residual"].describe(), width='stretch')
+
 
 
 
